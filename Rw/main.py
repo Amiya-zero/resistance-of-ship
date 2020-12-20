d91c66f61a4dc3e6e294b735ed1134ebef20e3b3
Amiya-zero <65345598+Amiya-zero@users.noreply.github.com>
2020-12-20 17:11:31 +0800

    Update main.py

--- Rw/main.py
+++ Rw/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-# author: Amiya   time: 2020/12/8
+# author: Gotthard   time: 2020/12/8
 # 快捷键 ctrl+D 复制一行 ctrl+Y 删除一行 Shift+TAB 删除缩进
 # TAB 缩进  Shift+Enter 换行
 # Ctrl+F 搜索 Ctrl+Shift+'-' 全部折叠 Ctrl+A 全选
